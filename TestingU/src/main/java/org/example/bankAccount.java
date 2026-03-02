ae9c5601946bc87c5ebd0e1003e56754fb6a16c2
ishach003 <choudharyisha2003@gmail.com>
2026-03-02 14:02:10 +0530

    Add files via upload

---
+++ TestingU/src/main/java/org/example/bankAccount.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class bankAccount {
+    private int balance;
+    public bankAccount(int balance){
+        this.balance=balance;
+    }
+    public void deposit(int amount){
+        balance+=amount;
+        System.out.println(balance);
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+    public void withdraw(int amount)  {
+        if(amount<=0) {
+            throw new IllegalArgumentException("Withdraw account must be positive");
+        }
+        if(amount>balance){
+            throw new IllegalArgumentException("Insufficient funds");
+        }
+        balance-=amount;
+        System.out.println(balance);
+    }
+
+}
